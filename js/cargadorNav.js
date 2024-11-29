8bd563d4b97955a7e3ca0ad5e7de9cf2b63252e7
Iván González <ivanfgonzalezruiz@gmail.com>
2024-11-29 13:15:34 -0300

    se encapsuló el nav, el footer y se dio funcionalidad al boton hamburguesa

---
+++ js/cargadorNav.js
@@ -0,0 +1,19 @@
+var contenedorNav = document.getElementById("cargadorNav");
+var encabezado = document.createElement("div");
+
+encabezado.innerHTML= ` <header>
+                        <h1>Todo Baratito...</h1>
+                    </header>
+                    <nav class="contenedorNav">
+                        <div id="hambur" onclick="desplegarMenu()"><i class="fa-solid fa-bars"></i></div>
+                        <div id="botonesNav">
+                            <button id="cerrarMenuVertical" class="navButtons" onclick="recomponerHamburguesa()">X</button>
+                            <div class="navButtons"><a href="index.html">Inicio</a></div>
+                            <div class="navButtons"><a href="productos.html">Productos</a></div>
+                            <div class="navButtons"><a href="resenia.html">Reseña</a></div>
+                            <div class="navButtons"><a href="formulario.html">Contacto</a></div>
+                            <div class="navButtons"><a href="carrito.html">Carrito</a></div>
+                        </div>
+                    </nav>`;
+
+contenedorNav.appendChild(encabezado);
--- js/funciones.js
+++ js/funciones.js
@@ -1,4 +1,4 @@
-function pivotHamburguesa()
+function desplegarMenu()
 {
     var hamburguesa = document.getElementById("hambur");
     var contenedorBotones = document.getElementById("botonesNav")
@@ -7,3 +7,26 @@ function pivotHamburguesa()
     contenedorBotones.style.display="flex";
 }
 
+function recomponerHamburguesa()
+{
+    var hamburguesa = document.getElementById("hambur");
+    var contenedorBotones = document.getElementById("botonesNav")
+
+    hamburguesa.style.display = "flex";
+    contenedorBotones.style.display="none";
+}
+
+function recomponerNavPantallaGrande(){
+    var contenedorBotones = document.getElementById("botonesNav");
+
+    if(window.innerWidth>700)
+    {
+        contenedorBotones.style.display="flex";
+    }
+    else
+    {
+        contenedorBotones.style.display="none";
+    }
+}
+
+window.addEventListener("resize", recomponerNavPantallaGrande);
